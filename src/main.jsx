e56bd97a79d84b463b47d8458ce080dae059125b
saikat <tawhidulislam3482@gmail.com>
2024-03-07 19:20:49 +0600

    react hot toster complete

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import {
-  
-  RouterProvider,
-} from "react-router-dom";
-import Routers from './Router/Routers';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import Routers from "./Router/Routers";
+import { Toaster } from "react-hot-toast";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-     <RouterProvider router={Routers} />
-  </React.StrictMode>,
-)
+    <Toaster 
+     position="top-right"
+    />
+    <RouterProvider router={Routers} />
+  </React.StrictMode>
+);
