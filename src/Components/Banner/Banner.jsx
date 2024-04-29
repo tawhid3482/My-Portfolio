56f301b170381cf60a93edf1418408ba87b5bafb
saikat <tawhidulislam3482@gmail.com>
2024-04-29 19:30:21 +0600

    bug fix

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import img from "../../assets/Saikat1-removebg-preview (2).png";
 import { FaArrowRight } from "react-icons/fa";
 
@@ -16,7 +17,9 @@ const Banner = () => {
           <h2 data-aos="fade-left" className="text-2xl lg:text-3xl text-yellow-600 font-bold">- I AM TAWHIDUL ISLAM </h2>
           <p className="text-2xl lg:text-3xl font-bold text-white ml-6">WEB DEVELOPER</p>
           <p className="text-white font-lg w-64 md:w-[450px] lg:w-[520px] text-left ">I am a React based front-end web developer focused on crafting clean & user-friendly experiences, I am passionate about building excellent software that improves the lives of those around me.</p>
-          <button className="btn uppercase bg-black text-white rounded-3xl ">more about me <FaArrowRight className="bg-yellow-600 rounded-full p-3 text-white text-4xl" /></button>
+          <Link to='/about'>
+          <button  className="btn uppercase bg-black text-white rounded-3xl ">more about me <FaArrowRight className="bg-yellow-600 rounded-full p-3 text-white text-4xl" /></button>
+          </Link>
         </div>
       </div>
     </div>
