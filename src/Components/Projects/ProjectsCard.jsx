569ae751cc587d15442b93bf027d43b6483a020e
saikat <tawhidulislam3482@gmail.com>
2024-05-11 18:19:12 +0600

    react project  skeleton

--- src/Components/Projects/ProjectsCard.jsx
+++ src/Components/Projects/ProjectsCard.jsx
@@ -1,28 +1,41 @@
+import { useEffect, useState } from "react";
 import { FaGithub, FaLocationArrow } from "react-icons/fa";
-import Skeleton from "react-loading-skeleton";
-import 'react-loading-skeleton/dist/skeleton.css'
+import ProjectSkeleton from "../Skeleton/ProjectSkeleton";
 
 
 const ProjectsCard = ({ project }) => {
   console.log(project);
   const { name, image, description,liveLink,code } = project;
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div>
-      <div data-aos="flip-left" className="card w-full lg:w-[520px] h-[400px] border border-yellow-600 bg-black text-white shadow-xl">
+      { 
+        loading ? <ProjectSkeleton></ProjectSkeleton>:
+        <div data-aos="flip-left" className="card w-full lg:w-[520px] h-[400px] border border-yellow-600 bg-black text-white shadow-xl">
         <figure>
-          <img src={image  || <Skeleton />} alt="projects Image" />
+          <img src={image} alt="projects Image" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">Project Name: {name  || <Skeleton />}</h2>
-          <p>{description  || <Skeleton />}</p>
+          <h2 className="card-title">Project Name: {name}</h2>
+          <p>{description }</p>
           <div className="card-actions mt-5 justify-between">
-            <a href={code  || <Skeleton />} target="blank" className="btn bg-yellow-600 text-white border-none">
+            <a href={code  } target="blank" className="btn bg-yellow-600 text-white border-none">
                 Code <FaGithub className="text-2xl"></FaGithub>
             </a>
-            <a href={liveLink  || <Skeleton />} target="blank" className="btn bg-yellow-600 text-white border-none">Live Demo <FaLocationArrow className="text-xl"></FaLocationArrow></a>
+            <a href={liveLink } target="blank" className="btn bg-yellow-600 text-white border-none">Live Demo <FaLocationArrow className="text-xl"></FaLocationArrow></a>
           </div>
         </div>
       </div>
+      }
+      
     </div>
   );
 };
---
+++ src/Components/Skeleton/ProjectSkeleton.jsx
@@ -0,0 +1,30 @@
+import Skeleton from "react-loading-skeleton";
+
+const ProjectSkeleton = ({ item }) => {
+  return Array(item)
+    .fill(0)
+    .map((items) => (
+      <div key={items.id}>
+        <div
+          data-aos="flip-left"
+          className="card w-full lg:w-[520px] h-[400px] border border-yellow-600 bg-black text-white shadow-xl"
+        >
+          <figure className="my-5">
+            <Skeleton width={450} height={300}></Skeleton>
+          </figure>
+          <div className="card-body ">
+            <Skeleton width={350} height={30}></Skeleton> 
+            <Skeleton width={350} height={10}></Skeleton> 
+
+            <div className="card-actions mt-5 justify-between">
+            <Skeleton width={80} height={40}></Skeleton> 
+            <Skeleton width={80} height={40}></Skeleton> 
+
+            </div>
+          </div>
+        </div>
+      </div>
+    ));
+};
+
+export default ProjectSkeleton;
