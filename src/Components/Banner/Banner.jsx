bded6754ab116231ad0076d305afee5677c9901f
saikat <tawhidulislam3482@gmail.com>
2024-05-11 17:34:11 +0600

    react skeleton banner

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -2,28 +2,25 @@ import { Link } from "react-router-dom";
 import img from "../../assets/Saikat1-removebg-preview (2).png";
 import { FaArrowRight } from "react-icons/fa";
 import Skeleton from "react-loading-skeleton";
-import 'react-loading-skeleton/dist/skeleton.css'
-import  { useState, useEffect } from 'react';
+import "react-loading-skeleton/dist/skeleton.css";
+import { useState, useEffect } from "react";
 
 const Banner = () => {
-
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 1000); 
+    }, 1000);
 
     return () => clearTimeout(timer);
   }, []);
 
-
   return (
-    
     <div className="">
       <div className="md:flex md:justify-around mt-8 p-3 ">
         <div data-aos="fade-right" className="">
-        {loading ? (
+          {loading ? (
             <Skeleton width={350} height={350} />
           ) : (
             <img
@@ -34,11 +31,45 @@ const Banner = () => {
           )}
         </div>
         <div className="mt-10 space-y-3 ml-5">
-          <h2 data-aos="fade-left" className="text-2xl lg:text-3xl text-yellow-600 font-bold">- I AM TAWHIDUL ISLAM </h2>
-          <p className="text-2xl lg:text-3xl font-bold text-white ml-6">WEB DEVELOPER</p>
-          <p className="text-white font-lg w-64 md:w-[450px] lg:w-[520px] text-left ">I am a React based front-end web developer focused on crafting clean & user-friendly experiences, I am passionate about building excellent software that improves the lives of those around me.</p>
-          <Link to='/about'>
-          <button  className="mt-4 btn uppercase bg-black text-white rounded-3xl ">more about me <FaArrowRight className="bg-yellow-600 rounded-full p-3 text-white text-4xl" /></button>
+          {loading ? (
+            <Skeleton width={355} height={38} />
+          ) : (
+            <h2
+              data-aos="fade-left"
+              className="text-2xl lg:text-3xl text-yellow-600 font-bold"
+            >
+              - I AM TAWHIDUL ISLAM
+            </h2>
+          )}
+
+          {loading ? (
+            <Skeleton width={300} height={38} />
+          ) : (
+            <p className="text-2xl lg:text-3xl font-bold text-white ml-6">
+              WEB DEVELOPER
+            </p>
+          )}
+
+          {loading ? (
+            <Skeleton count={3} width={400} height={15} />
+          ) : (
+            <p className="text-white font-lg w-64 md:w-[450px] lg:w-[520px] text-left ">
+              I am a React based front-end web developer focused on crafting
+              clean & user-friendly experiences, I am passionate about building
+              excellent software that improves the lives of those around me.
+            </p>
+          )}
+
+             <p className="text-white font-lg w-64 md:w-[450px] lg:w-[520px] text-left ">
+            I am a React based front-end web developer focused on crafting clean
+            & user-friendly experiences, I am passionate about building
+            excellent software that improves the lives of those around me.
+          </p>
+          <Link to="/about">
+            <button className="mt-4 btn uppercase bg-black text-white rounded-3xl ">
+              more about me{" "}
+              <FaArrowRight className="bg-yellow-600 rounded-full p-3 text-white text-4xl" />
+            </button>
           </Link>
         </div>
       </div>
